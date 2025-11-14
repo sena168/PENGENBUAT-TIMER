6f5fff134514667324def67ec848827009994c92
sena168 <suhuac3ng@gmail.com>
2025-11-15 02:01:01 +0700

    Update prompt-list to mark all tasks as completed

--- externals/random-title-config.js
+++ externals/random-title-config.js
@@ -3,7 +3,7 @@ const randomTitleConfig = {
   // Array of possible titles
   possibleTitles: [
     'PENGENBUAT',
-    'RASA-YANGPERNAHADA',
+    'RASAYANG-PERNAHADA',
     'SEMENJAK-DULUKALA',
     'PENGENTAPI-BELUMKESAMPAIAN',
     'INILAH',
@@ -18,16 +18,24 @@ const randomTitleConfig = {
     'KALAU-INI',
     'TIMERNYA',
     'BEGITULAH',
+    'LUPANAMA-INGATRASA',
+    'MASIHINGATKAH-PADAKU',
+    'SEANDAINYA-WAKTUITU',
+    'INIDULU-CURHATBELAKANGAN',
+    'OTEWE',
+    'KENAPA-YA',
+    'SULIT-MELUPAKAN',
+    '',
   ],
-  
+
   // Update interval in milliseconds (5 minutes)
   updateInterval: 300000,
-  
+
   // Function to get a random title
-  getRandomTitle: function() {
+  getRandomTitle: function () {
     const randomIndex = Math.floor(Math.random() * this.possibleTitles.length);
     return this.possibleTitles[randomIndex];
-  }
+  },
 };
 
 // Export for use in other modules (if using modules)
