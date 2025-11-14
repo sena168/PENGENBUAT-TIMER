61f0c1fa2da2970718025683848864bdac225c79
sena168 <suhuac3ng@gmail.com>
2025-11-15 02:06:39 +0700

    Move random-title files out of externals folder and add additional titles

---
+++ random-title-config.js
@@ -0,0 +1,52 @@
+// Configuration for random title functionality
+const randomTitleConfig = {
+  // Array of possible titles
+  possibleTitles: [
+    'PENGENBUAT',
+    'RASAYANG-PERNAHADA',
+    'SEMENJAK-DULUKALA',
+    'PENGENTAPI-BELUMKESAMPAIAN',
+    'INILAH',
+    'MUNGKIN',
+    'ISENGISENG',
+    'HASRATYANG-TERWUJUD',
+    'ADALAH',
+    'HANYALAH-SEBUAH',
+    'INI',
+    'BENARKAH?',
+    'YUKS',
+    'KALAU-INI',
+    'TIMERNYA',
+    'BEGITULAH',
+    'LUPANAMA-INGATRASA',
+    'MASIHINGATKAH-PADAKU',
+    'SEANDAINYA-WAKTUITU',
+    'INIDULU-CURHATBELAKANGAN',
+    'OTEWE',
+    'KENAPA-YA',
+    'SULIT-MELUPAKAN',
+    'SUNGGUHNYA',
+    'ENTAH',
+    'KEMUNGKINAN',
+    'AWALNYA',
+    'SEPERTINYA',
+    'MESTINYA',
+    'APAKAH',
+    'DAPAT',
+    'MENJADI',
+  ],
+
+  // Update interval in milliseconds (5 minutes)
+  updateInterval: 300000,
+
+  // Function to get a random title
+  getRandomTitle: function () {
+    const randomIndex = Math.floor(Math.random() * this.possibleTitles.length);
+    return this.possibleTitles[randomIndex];
+  },
+};
+
+// Export for use in other modules (if using modules)
+if (typeof module !== 'undefined' && module.exports) {
+  module.exports = randomTitleConfig;
+}
